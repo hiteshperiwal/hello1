3a42704c70ae1c197782e0b5ade9673d28d41da3
hiteshperiwal <119793293+hiteshperiwal@users.noreply.github.com>
2023-04-10 21:09:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKVIANS')
+  res.send('Hello MCKVIANS....how are you')
 })
 
 app.listen(4000, ()=>{
